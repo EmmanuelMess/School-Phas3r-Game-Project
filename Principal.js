84a945db0c5b13754e7d3eb9a896c537eb245198
Emmanuel M <emmanuelbendavid@gmail.com>
2018-04-30 22:16:47 -0300

    Some corrections to animations

--- Principal.js
+++ Principal.js
@@ -7,12 +7,15 @@ let marco;//el personaje
 let mapa;
 let layer;
 
+const ANIMATION_CORRER = "correr anim";
+const ANIMATION_QUIETO = "quieto anim";
+
 const ESTADO = {
     preload: function () {//sube todo
         JUEGO.load.tilemap('mario', 'assets/super_mario.json', null, Phaser.Tilemap.TILED_JSON);
         JUEGO.load.image('fondoterreno', 'assets/fondo.png');//cargo mi imagen
         JUEGO.load.spritesheet('Correr', 'assets/Correr.png', 31.75, 40);//cargo mi conjunto de imagenes para marco y la nombro MarcoRun
-        JUEGO.load.spritesheet('Quieto', 'assets/Quieto.png', 30, 40);
+        JUEGO.load.spritesheet('Quieto', 'assets/Quieto.png', 30, 40, 3);
     },
 
     create: function () {//se arranca solo una vez como un constructor  mas o menos
@@ -20,19 +23,17 @@ const ESTADO = {
 
         tileSprite = JUEGO.add.tileSprite(0, 0, 800, 220, 'fondoterreno');//muestro por pantalla el terreno dandole los limites laterales, superior e inferior + el objeto
         cursors = JUEGO.input.keyboard.createCursorKeys();//guardo en cursors lo que se presiona por teclado
-        
-        //marco=JUEGO.add.sprite(50,160,'Correr');//asigno y muestro mi conjunto de imagenes en MarcoRun
+
         marco = JUEGO.add.sprite(50, 160, 'Quieto');//no podes tener 2 animations.play ni dos add.sprite se superponen
         JUEGO.physics.enable(marco);
         JUEGO.physics.arcade.gravity.y = 0;
 
-        marco.animations.add('CorreR', [0, 2, 3, 4, 5, 6], 10, true);//Agarro solo un par de frames porque los faltantes estan mal cortados CorreR = [0,2,3,4,5,6] frames de Correr
-        marco.animations.add('QuietO', [0, 1, 2], 3, true);
+        marco.animations.add(ANIMATION_QUIETO, [0, 0, 1, 1, 2, 2], 7, true);
+        marco.animations.add(ANIMATION_CORRER, [0, 2, 3, 4, 5, 6], 7, true);//Agarro solo un par de frames porque los faltantes estan mal cortados CorreR = [0,2,3,4,5,6] frames de Correr
     },
 
     update: function () {//se verifica frame a frame izi
-        //  marco.animations.play('CorreR');
-        marco.animations.play('QuietO');//al no poder tener 2 animations.play solo corro por defecto la QuietO y de ahi me muevo QuietO es la padre
+        marco.animations.play(ANIMATION_QUIETO);//al no poder tener 2 animations.play solo corro por defecto la QuietO y de ahi me muevo QuietO es la padre
         marco.body.velocity.x = 0;
 
         if (cursors.up.isDown) {
@@ -42,6 +43,7 @@ const ESTADO = {
         }
 
         if (cursors.right.isDown && marco.x <= 770) {
+            marco.animations.play(ANIMATION_CORRER);
             marco.x++;
             if (marco.x >= 600 && limitesTerreno < 460) {
                 tileSprite.tilePosition.x -= 6;
