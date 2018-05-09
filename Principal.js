ffdf26297438ffcdf51ff9fc3b6fe77e1e43b043
Franco Vallejos Vigier <francovallejosvigier@gmail.com>
2018-05-09 18:41:10 -0300

    Used classes

--- Principal.js
+++ Principal.js
@@ -1,64 +1,95 @@
-const JUEGO = new Phaser.Game(800, 220, Phaser.CANVAS, 'bloque_juego');
+var juego = new Phaser.Game(800,220,Phaser.CANVAS,'bloque_juego');
+var cursorparamover;//para mover el terreno conforme se aprieta las flechitas 
+var tileSprite;//para actualizar el movimiento del terreno, darle macarena
+var limitesTerreno=0;//pa que no se salga de los limites del nivel
+var Marco;//el personaje
+var Mapa;
+var Layer;
+var Mar;
+ class MarcoPlayer extends Phaser.Sprite{
+    constructor(juego, x, y, rotateSpeed) {
+        super(juego, x, y, 'Quieto');
+        this.animations.add('Quiet', [0, 1, 2], 3, true);
+    }
+   Mover() {
+        this.x += 1;
+    }
+     update() {
+         this.animations.play('Quiet');
+        //this.Mover();
+    }
+}
+;
+var Estado = {
+  preload: function(){//sube todo
+      juego.load.tilemap('mario','assets/super_mario.json',null,Phaser.Tilemap.TILED_JSON);
+      juego.load.image('fondoterreno','assets/fondo.png');//cargo mi imagen
+      juego.load.spritesheet('Correr','assets/Correr.png',31.75,40);//cargo mi conjunto de imagenes para marco y la nombro MarcoRun
+      juego.load.spritesheet('Quieto', 'assets/Quieto.png', 30, 40);
+      juego.load.image('Quieto2', 'assets/Quieto.png');
+  },
+  
+  create: function(){//se arranca solo una vez como un constructor  mas o menos
+      juego.physics.startSystem(Phaser.Physics.ARCADE);
 
-let cursorParaMover;//para mover el terreno conforme se aprieta las flechitas
-let tileSprite;//para actualizar el movimiento del terreno, darle macarena
-let limitesTerreno = 0;//pa que no se salga de los limites del nivel
-let marco;//el personaje
-let mapa;
-let layer;
+      tileSprite=juego.add.tileSprite(0,0,800,220,'fondoterreno');//muestro por pantalla el terreno dandole los limites laterales, superior e inferior + el objeto
+      cursors = juego.input.keyboard.createCursorKeys();//guardo en cursors lo que se presiona por teclado
 
-const ANIMATION_CORRER = "correr anim";
-const ANIMATION_QUIETO = "quieto anim";
+      Mar = new MarcoPlayer(juego, 25, 160, 1);
+      
+      juego.add.existing(Mar);
 
-const ESTADO = {
-    preload: function () {//sube todo
-        JUEGO.load.tilemap('mario', 'assets/super_mario.json', null, Phaser.Tilemap.TILED_JSON);
-        JUEGO.load.image('fondoterreno', 'assets/fondo.png');//cargo mi imagen
-        JUEGO.load.spritesheet('Correr', 'assets/Correr.png', 31.75, 40);//cargo mi conjunto de imagenes para marco y la nombro MarcoRun
-        JUEGO.load.spritesheet('Quieto', 'assets/Quieto.png', 30, 40, 3);
-    },
+      //Marco=juego.add.sprite(50,160,'Correr');//asigno y muestro mi conjunto de imagenes en MarcoRun
+      Marco=juego.add.sprite(50,160,'Quieto');//no podes tener 2 animations.play ni dos add.sprite se superponen
+      juego.physics.enable(Marco);
+      juego.physics.enable(Mar);
 
-    create: function () {//se arranca solo una vez como un constructor  mas o menos
-        JUEGO.physics.startSystem(Phaser.Physics.ARCADE);
+      juego.physics.arcade.gravity.y = 0;
+      
 
-        tileSprite = JUEGO.add.tileSprite(0, 0, 800, 220, 'fondoterreno');//muestro por pantalla el terreno dandole los limites laterales, superior e inferior + el objeto
-        cursors = JUEGO.input.keyboard.createCursorKeys();//guardo en cursors lo que se presiona por teclado
+     // for (var i = 0; i < 100; i++) {
+     //     Mar.Mover();
+     // }
+      Marco.animations.add('CorreR',[0,2,3,4,5,6],10,true);//Agarro solo un par de frames porque los faltantes estan mal cortados CorreR = [0,2,3,4,5,6] frames de Correr
+      Marco.animations.add('QuietO',[0,1,2],3,true);
+  },  
+  
+  update:function(){//se verifica frame a frame izi
+      //  Marco.animations.play('CorreR');
+  
+    Marco.animations.play('QuietO');//al no poder tener 2 animations.play solo corro por defecto la QuietO y de ahi me muevo QuietO es la padre
+      Marco.body.velocity.x = 0;
+     
 
-        marco = JUEGO.add.sprite(50, 160, 'Quieto');//no podes tener 2 animations.play ni dos add.sprite se superponen
-        JUEGO.physics.enable(marco);
-        JUEGO.physics.arcade.gravity.y = 0;
-
-        marco.animations.add(ANIMATION_QUIETO, [0, 0, 1, 1, 2, 2], 7, true);
-        marco.animations.add(ANIMATION_CORRER, [0, 2, 3, 4, 5, 6], 7, true);//Agarro solo un par de frames porque los faltantes estan mal cortados CorreR = [0,2,3,4,5,6] frames de Correr
-    },
-
-    update: function () {//se verifica frame a frame izi
-        marco.animations.play(ANIMATION_QUIETO);//al no poder tener 2 animations.play solo corro por defecto la QuietO y de ahi me muevo QuietO es la padre
-        marco.body.velocity.x = 0;
-
-        if (cursors.up.isDown) {
-            if (marco.body.onFloor()) {
-                marco.body.velocity.y = -200;
-            }
-        }
-
-        if (cursors.right.isDown && marco.x <= 770) {
-            marco.animations.play(ANIMATION_CORRER);
-            marco.x++;
-            if (marco.x >= 600 && limitesTerreno < 460) {
-                tileSprite.tilePosition.x -= 6;
-                marco.x--;
-                limitesTerreno++;
-            }
-            if (limitesTerreno >= 460) {
-                marco.x++;
-            }
-        }
-        if (cursors.left.isDown && marco.x !== 10) {
-            marco.x--;
+      if (cursors.up.isDown)
+      {
+         
+          
+          
+        if (Marco.body.onFloor())
+        {
+            Marco.body.velocity.y = -200;
         }
     }
+
+      if (cursors.right.isDown && Marco.x <= 770) {
+          Marco.x += 1; Mar.Mover();
+       if(Marco.x>=600&&limitesTerreno<460){
+            tileSprite.tilePosition.x -=6;
+            Marco.x--;
+           limitesTerreno++;
+           
+       }
+       if(limitesTerreno>=460){
+           Marco.x++;
+       }
+       } if(cursors.left.isDown&&Marco.x!==10){
+       Marco.x--;
+   }
+   }
+    
+  
 };
 
-JUEGO.state.add('principal', ESTADO);
-JUEGO.state.start('principal');
+juego.state.add('principal', Estado);
+juego.state.start('principal');
