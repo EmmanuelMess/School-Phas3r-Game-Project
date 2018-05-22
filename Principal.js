66a7bdff34b8bb1a7397838435ef090bb24944fb
Emmanuel M <emmanuelbendavid@gmail.com>
2018-05-22 10:24:35 -0300

    Added floor

--- Principal.js
+++ Principal.js
@@ -1,3 +1,4 @@
+const GRAVEDAD = 250;
 const juego = new Phaser.Game(800, 220, Phaser.CANVAS, 'bloque_juego');
 
 let tileSprite;//para actualizar el movimiento del terreno, darle macarena
@@ -6,25 +7,31 @@ let Mapa;
 let Layer;
 let Marco;
 let Cursors;
+let Piso;
 
 class MarcoPlayer extends Phaser.Sprite {
     constructor(juego, x, y) {
         super(juego, x, y, 'Quieto');
+    }
+
+    load() {
         this.animations.add('Quiet', [0, 1, 2], 3, true);
-        /*
-        Marco.animations.add('CorreR', [0, 2, 3, 4, 5, 6], 10, true);//Agarro solo un par de frames porque los faltantes estan mal cortados CorreR = [0,2,3,4,5,6] frames de Correr
-        Marco.animations.add('QuietO', [0, 1, 2], 3, true);
-        */
+        this.body.collideWorldBounds = true;
+
+        //FIXUP: Agarro solo un par de frames porque los faltantes estan mal cortados CorreR = [0,2,3,4,5,6] frames de Correr
+        //Marco.animations.add('CorreR', [0, 2, 3, 4, 5, 6], 10, true);
     }
 
     mover(cursors) {
-        if (cursors.up.isDown) {
-            if (this.body.onFloor()) {
-                this.body.velocity.y = -200;
-            }
+        if (cursors.up.isDown && this.body.touching.down) {
+            this.body.velocity.y = -200;
         }
 
-        if (cursors.right.isDown && Marco.x <= 770) {
+        if(this.y >= 200) {
+            this.body.velocity.y = 0;
+        }
+
+        if (cursors.right.isDown && this.x <= 770) {
             this.x++;
             if (this.x >= 600 && limitesTerreno < 460) {
                 tileSprite.tilePosition.x -= 6;
@@ -50,6 +57,7 @@ class MarcoPlayer extends Phaser.Sprite {
 let Estado = {
     preload: function () {//sube todo
         juego.load.tilemap('mario', 'assets/super_mario.json', null, Phaser.Tilemap.TILED_JSON);
+        juego.load.image('piso', 'assets/piso.png');
         juego.load.image('fondoterreno', 'assets/fondo.png');//cargo mi imagen
         juego.load.spritesheet('Correr', 'assets/Correr.png', 31.75, 40);//cargo mi conjunto de imagenes para marco y la nombro MarcoRun
         juego.load.spritesheet('Quieto', 'assets/Quieto.png', 30, 40);
@@ -62,15 +70,28 @@ let Estado = {
         tileSprite = juego.add.tileSprite(0, 0, 800, 220, 'fondoterreno');//muestro por pantalla el terreno dandole los limites laterales, superior e inferior + el objeto
         Cursors = juego.input.keyboard.createCursorKeys();//guardo en Cursors lo que se presiona por teclado
 
-        Marco = new MarcoPlayer(juego, 25, 160, 1);
+        juego.physics.arcade.gravity.y = GRAVEDAD;
+
+        Marco = new MarcoPlayer(juego, 35, 160, 1);
 
         juego.add.existing(Marco);
         juego.physics.enable(Marco);
 
-        juego.physics.arcade.gravity.y = 0;
+        Marco.load();
+
+        // Creo el piso
+        Piso = juego.add.group();
+        for(let x = 0; x < juego.width; x += 32) {
+            let segmentoPiso = juego.add.sprite(x, juego.height - 20, 'piso');
+            juego.physics.enable(segmentoPiso, Phaser.Physics.ARCADE);
+            segmentoPiso.body.immovable = true;
+            segmentoPiso.body.allowGravity = false;
+            Piso.add(segmentoPiso);
+        }
     },
 
     update: function () {//se verifica frame a frame izi
+        juego.physics.arcade.collide(Marco, Piso);
         Marco.updateSpecial(Cursors);
     }
 };
